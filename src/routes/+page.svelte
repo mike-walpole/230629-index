bb85e8e51a912016a0093fc26b413bc3570eac2d
mike-walpole <walpole.mike@gmail.com>
2023-07-02 09:32:14 +0200

    question v0.1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,14 +10,20 @@
 	>
 		<source src="/intro.webm" type="video/webm" />
 	</video>
-	<img class="w-28 md:w-56 pl-5 md:pl-10 pt-5 md:pt-10" src="/digitalindexlogo.avif" alt="logo" />
+	<img
+		class="w-28 md:w-56 pl-5 md:pl-10 pt-5 md:pt-10"
+		src="/digitalindexbluepineapple.avif"
+		alt="logo"
+	/>
 	<div class="absolute bottom-0 left-0 w-3/4 md:w-2/3 pl-5 md:pl-10 pb-20 md:pb-10">
 		<h1 class="text-5xl font-soehne md:text-8xl leading-20 text-white">
 			Measuring digital excellence of Australian companies
 		</h1>
-		<button class="text-white font-soehne my-5 px-5 py-2 border border-white text-xl font-bold"
-			>Show me how I compare</button
-		>
+		<a href="/digital"
+			><button class="text-white font-soehne my-5 px-5 py-2 border border-white text-xl font-bold"
+				>Show me how I compare</button
+			>
+		</a>
 	</div>
 </section>
 
@@ -29,16 +35,12 @@
 			</div>
 			<div>
 				<p class="col-span-1 text-white font-soehne">
-					The world’s most digitally mature companies lead all other companies in value creation.
-					They also have proved much more resilient during the crisis. After their market valuations
-					took a big hit at the start of the pandemic, these companies quickly rebounded. Within six
-					months, their valuations were, on average, 23% above precrisis levels, while the least
-					digitally mature companies had managed just 7% growth, on average. Companies aiming to
-					accelerate their digital transformation and create value should learn how the most
-					digitally mature companies charted a path to success in areas such as operational
-					efficiency, shareholder value, and top-line growth. We have identified four accelerators
-					that are strongly tied to the ability of these companies to translate strategy into
-					outcomes—and market dominance.
+					The most digitally mature companies lead all other companies in value creation. They also
+					have proved much more resilient during the crisis. Companies aiming to accelerate their
+					digital transformation and create value should learn how the most digitally mature
+					companies charted a path to success in areas such as operational efficiency, shareholder
+					value, and top-line growth. We have identified four accelerators that are strongly tied to
+					the ability of these companies to translate strategy into outcomes—and market dominance.
 				</p>
 				<button class="border border-white text-white my-5 px-5 py-2 font-bold">
 					Show me how I compare</button
@@ -53,39 +55,54 @@
 			<div class="col-span-1 md:w-2/3 text-white font-heldane text-5xl pl-2 mb-5 md:mb-0">
 				What do we measure
 			</div>
-			<p class="col-span-1 text-white font-soehne">
-				The world’s most digitally mature companies lead all other companies in value creation. They
-				also have proved much more resilient during the crisis. After their market valuations took a
-				big hit at the start of the pandemic, these companies quickly rebounded. Within six months,
-				their valuations were, on average, 23% above precrisis levels, while the least digitally
-				mature companies had managed just 7% growth, on average. Companies aiming to accelerate
-				their digital transformation and create value should learn how the most digitally mature
-				companies charted a path to success in areas such as operational efficiency, shareholder
-				value, and top-line growth. We have identified four accelerators that are strongly tied to
-				the ability of these companies to translate strategy into outcomes—and market dominance.
-			</p>
+			<div class="text-white font-soehne col-span-1">
+				<p class=" text-white font-soehne">
+					The Digital Index evaluates digital capability across 5 clearly defined business
+					dimensions to create a holistic view of digital maturity across the organisation.
+					Providing an experience where <span class="underline">customers </span>view the
+					organization as their digital partner using their preferred channels of interaction to
+					control their connected future on and offline
+				</p>
+				<p class="mt-2">
+					<span class="underline">Strategy</span> focuses on how the business transforms or operates
+					to increase its competitive advantage through digital initiatives; it is embedded within the
+					overall business strategy
+				</p>
+				<p class="mt-2">
+					<span class="underline">Technology</span> underpins the success of digital strategy by helping
+					to create, process, store, secure and exchange data to meet the needs of customers at low cost
+					and low overheads
+				</p>
+				<p class="mt-2">
+					Operational capability to <span class="underline">execute</span> and evolving processes and
+					tasks by utilizing digital technologies to drive strategic management and enhance business
+					efficiency and effectiveness.
+				</p>
+				<p class="mt-2">
+					Defining and developing an <span class="underline">organizational culture</span> with governance
+					and talent processes to support progress along the digital maturity curve, and the flexibly
+					to achieve growth and innovation objectives
+				</p>
+			</div>
 		</div>
 		<div class="grid grid-cols-1 md:grid-cols-2 gap-x-24 my-10">
-			<div>
+			<div class="mb-10">
 				<p class="text-white font-soehne underline mb-5">Completness of Vision</p>
 				<img class="w-full my-5 hover:grayscale-0" src="/one.avif" alt="one" />
 				<p class="text-white font-soehne">
-					The world’s most digitally mature companies lead all other companies in value creation.
-					They also have proved much more resilient during the crisis. After their market valuations
-					took a big hit at the start of the pandemic, these companies quickly rebounded. Within six
-					months, their valuations were, on average, 23% above precrisis levels, while the least
-					digitally mature companies had managed just 7% growth, on average.
+					Companies that are characterized by strategic activities due to digitalization of products
+					and services as well as the definition of new digital business models. Leading companies
+					create complex digital ecosystems to achieve new services for their clients.
 				</p>
 			</div>
 			<div>
 				<p class="text-white font-soehne underline mb-5">Operational Excellence</p>
 				<img class="w-full my-5 hover:grayscale-0" src="/three.avif" alt="one" />
 				<p class="text-white font-soehne">
-					The world’s most digitally mature companies lead all other companies in value creation.
-					They also have proved much more resilient during the crisis. After their market valuations
-					took a big hit at the start of the pandemic, these companies quickly rebounded. Within six
-					months, their valuations were, on average, 23% above precrisis levels, while the least
-					digitally mature companies had managed just 7% growth, on average.
+					Companies can be characterized by a strong ability to operationalize a digital strategy
+					considering their core value chain and available resources. Leading companies build upon
+					digital capabilities, set up IT backbones and increase the intelligence of their business
+					functions.
 				</p>
 			</div>
 		</div>
@@ -96,50 +113,53 @@
 			<div class="col-span-1 md:w-2/3 text-white font-heldane text-5xl pl-2 mb-5 md:mb-0">
 				Why should I do it
 			</div>
-			<p class="col-span-1 text-white font-soehne">
-				The world’s most digitally mature companies lead all other companies in value creation. They
-				also have proved much more resilient during the crisis. After their market valuations took a
-				big hit at the start of the pandemic, these companies quickly rebounded. Within six months,
-				their valuations were, on average, 23% above precrisis levels, while the least digitally
-				mature companies had managed just 7% growth, on average. Companies aiming to accelerate
-				their digital transformation and create value should learn how the most digitally mature
-				companies charted a path to success in areas such as operational efficiency, shareholder
-				value, and top-line growth. We have identified four accelerators that are strongly tied to
-				the ability of these companies to translate strategy into outcomes—and market dominance.
-			</p>
+			<div class="text-white font-soehne">
+				<p class="col-span-1 text-white font-soehne">
+					Digital transformation has taken a top spot on leaders’ agendas for several years, but the
+					current economic backdrop has accelerated its urgency. Companies cannot go back to
+					business as usual. Future competitiveness and resiliency will depend on maximizing the
+					value from digital transformations. Regardless of where a company is on its digital
+					journey, the four accelerators can help continue the vital work of transforming it into a
+					bionic company.
+				</p>
+				<p class="mt-2">Attaining the bionic company level is driven by four key accelerators:</p>
+				<ol>
+					<li>a) Investing Significantly in Technology, Data, and Human Capabilities</li>
+					<li>b) Putting AI at the Core of the Digital Transformation</li>
+					<li>c) Establishing Governance and Adopting a Platform Operating Model</li>
+					<li>d) Connecting Technology and Human Capabilities</li>
+				</ol>
+			</div>
 		</div>
 		<div class="grid grid-cols-1 md:grid-cols-3 gap-x-24 my-10">
-			<div>
-				<p class="text-white font-soehne underline mb-5">Completness of Vision</p>
+			<div class="mb-10">
+				<p class="text-white font-soehne underline mb-5">
+					Determine where your company stands in its digital transformation
+				</p>
 				<img class="w-full my-5 hover:grayscale-0" src="/eight.avif" alt="one" />
 				<p class="text-white font-soehne">
-					The world’s most digitally mature companies lead all other companies in value creation.
-					They also have proved much more resilient during the crisis. After their market valuations
-					took a big hit at the start of the pandemic, these companies quickly rebounded. Within six
-					months, their valuations were, on average, 23% above precrisis levels, while the least
-					digitally mature companies had managed just 7% growth, on average.
+					Assess the current maturity of digital capabilities, evaluate the status of existing
+					digital initiatives, and review long-term transformation goals.
 				</p>
 			</div>
-			<div>
-				<p class="text-white font-soehne underline mb-5">Operational Excellence</p>
+			<div class="mb-10">
+				<p class="text-white font-soehne underline mb-5">
+					Discuss the opportunities for and the value of applying the four accelerators
+				</p>
 				<img class="w-full my-5 hover:grayscale-0" src="/six.avif" alt="one" />
 				<p class="text-white font-soehne">
-					The world’s most digitally mature companies lead all other companies in value creation.
-					They also have proved much more resilient during the crisis. After their market valuations
-					took a big hit at the start of the pandemic, these companies quickly rebounded. Within six
-					months, their valuations were, on average, 23% above precrisis levels, while the least
-					digitally mature companies had managed just 7% growth, on average.
+					Consider how each of the four accelerators can help the company’s unique journey on the
+					basis of its current capabilities, initiatives, and goals.
 				</p>
 			</div>
 			<div>
-				<p class="text-white font-soehne underline mb-5">Operational Excellence</p>
+				<p class="text-white font-soehne underline mb-5">
+					Start pilots and agree on a roadmap to accelerate the digital transformation.
+				</p>
 				<img class="w-full my-5 hover:grayscale-0" src="/seven.avif" alt="one" />
 				<p class="text-white font-soehne">
-					The world’s most digitally mature companies lead all other companies in value creation.
-					They also have proved much more resilient during the crisis. After their market valuations
-					took a big hit at the start of the pandemic, these companies quickly rebounded. Within six
-					months, their valuations were, on average, 23% above precrisis levels, while the least
-					digitally mature companies had managed just 7% growth, on average.
+					Depending upon how the company wants to leverage the four accelerators, design pilots that
+					can advance the digital journey and attain the desired goals.
 				</p>
 			</div>
 		</div>
@@ -148,18 +168,19 @@
 			>Show me how I compare</button
 		>
 		<div class="grid grid-cols-3 md:grid-cols-6 text-white font-soehne mt-48 pb-32">
-			<img class="w-20" src="/digitalindexlogo.avif" alt="logo" />
+			<img class="w-20 mb-5" src="/digitalindexbluepineapple.avif" alt="logo" />
 			<div />
 			<div />
-			<div>
-				<p class="font-bold mb-1">Clouds</p>
-				<p>AWS</p>
-				<p>Azure</p>
-			</div>
+
 			<div>
 				<p class="font-bold mb-1">Platforms</p>
 				<p>Salesforce</p>
 				<p>ServiceNow</p>
+			</div>
+			<div>
+				<p class="font-bold mb-1">Clouds</p>
+				<p>AWS</p>
+				<p>Azure</p>
 			</div>
 
 			<div>
---
+++ src/routes/digital/+page.svelte
@@ -0,0 +1,255 @@
+<script>
+	import Select from 'svelte-select';
+	let box;
+	let submitted = false;
+	let items = [{ label: '1-5' }, { label: '5-20' }, { label: '20-50' }, { label: '50+' }];
+
+	let titles = [
+		{ label: 'Junior Lecturer (without PhD)' },
+		{ label: 'Junior Lecturer (with PhD)' },
+		{ label: 'PhD candidate (with teaching obligations)' },
+		{ label: 'Post-Doc (with teaching obligations)' },
+		{ label: 'Assistant Professor' },
+		{ label: 'Associate Professor' },
+		{ label: 'Full Professor' },
+		{ label: 'Other' }
+	];
+
+	let types = [
+		{ label: 'Publicly funded University' },
+		{ label: 'Privately funded University' },
+		{ label: 'Liberal Arts College' },
+		{ label: 'University of Applied Sciences' },
+		{ label: 'Other' }
+	];
+	let consent = false;
+	let institution = '';
+	let selected = '';
+	let position = '';
+	let q1c = '';
+	let q2 = '';
+	let q3 = '';
+	let q4a = '';
+	let q4b = '';
+	let q4c = '';
+	let q4d = '';
+	let q4e = '';
+	let q4f = '';
+	let q4g = '';
+	let q5 = '';
+	let q6a = '';
+	let q6b = '';
+	let q6c = '';
+	let q6d = '';
+	let q6e = '';
+	let q6f = '';
+	let q6g = '';
+	let q6h = '';
+	let q7a = '';
+	let q7b = '';
+	let q7c = '';
+	let q7d = '';
+	let q7e = '';
+	let q7f = '';
+	let q7g = '';
+	let q7h = '';
+	let q7i = '';
+	let q7j = '';
+	let q7k = '';
+	let q9 = '';
+	let q10 = '';
+	let q11 = '';
+	let q12 = '';
+	let q13a = '';
+	let q13b = '';
+	let q13c = '';
+	let q13d = '';
+	let q13e = '';
+	let q13f = '';
+	let q13g = '';
+	let q13h = '';
+	let q13i = '';
+	let q13j = '';
+	let q13k = '';
+
+	let email = '';
+
+	let created = new Date();
+
+	async function handleSubmit() {
+		submitted = !submitted;
+
+		const data = {
+			selected,
+			q1c,
+			q2,
+			q3,
+			q4a,
+			q4b,
+			q4c,
+			q4d,
+			q4e,
+			q4f,
+			q4g,
+			q5,
+			q6a,
+			q6b,
+			q6c,
+			q6d,
+			q6e,
+			q6f,
+			q6g,
+			q6h,
+			q7a,
+			q7b,
+			q7c,
+			q7d,
+			q7e,
+			q7f,
+			q7g,
+			q7h,
+			q7i,
+			q7j,
+			q7k,
+			q9,
+			q10,
+			q11,
+			q12,
+			q13a,
+			q13b,
+			q13c,
+			q13d,
+			q13e,
+			q13f,
+			q13g,
+			q13h,
+			q13i,
+			q13j,
+			q13k,
+			consent,
+
+			email,
+			created,
+			institution,
+			position
+		};
+
+		fetch('https://sheet.best/api/sheets/8a267e1c-3bae-4693-b18a-f9ed9e29c7e4/tabs/Rawb', {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(data)
+		})
+			.then((r) => r.json())
+			.then((data) => {
+				// The response comes here
+				console.log(data);
+			})
+			.catch((error) => {
+				// Errors are reported there
+				console.log(error);
+			});
+	}
+</script>
+
+<div id="top" class="bg-[#1D1C1C] flex w-screen justify-center">
+	<div class="w-full text-white font-soehne md:max-w-2xl">
+		<img class="mx-10 my-10 w-32 mr-5" src="/digitalindexbluepineapple.avif" alt="logo" />
+		<form class="w-full" on:submit|preventDefault={handleSubmit}>
+			<div class="mt-10 mx-5 md:ml-10">
+				<p class="text-slate-500">Question 1</p>
+				<h2 class="text-xl font-bold mb-5">My company has this many employees</h2>
+				<Select class="text-black" id="countries" {items} bind:value={selected} />
+			</div>
+
+			<div class="mt-10 mx-2 mr-10 md:ml-10">
+				<p class="text-slate-500">Question 2</p>
+				<h2 class="text-xl font-bold">
+					My company prioritizes digital excellence in its strategic documents or discussions and
+					communicates frequently about it
+				</h2>
+				<p class="italic text-slate-500 mb-5">
+					(Frequency: 1 Never 2 Rarely 3 Occasionally 4 Frequently 5 All the time)
+				</p>
+				<div class="flex flex-row">
+					<input type="radio" id="q11" name="q2" value="1" bind:group={q2} />
+					<label for="q11">1</label>
+					<input type="radio" id="q12" name="q2" value="2" bind:group={q2} />
+					<label for="q12">2</label>
+					<input type="radio" id="q13" name="q2" value="3" bind:group={q2} />
+					<label for="q13">3</label>
+					<input type="radio" id="q14" name="q2" value="4" bind:group={q2} />
+					<label for="q14">4</label>
+					<input type="radio" id="q15" name="q2" value="5" bind:group={q2} />
+					<label for="q15">5</label>
+				</div>
+			</div>
+
+			<div class="mt-10 mx-2 md:ml-10">
+				<p class="text-slate-500">Question 3</p>
+				<h2 class="text-xl font-bold">
+					I have a clear understanding of what digital excellence is and entails
+				</h2>
+				<p class="italic text-slate-500 mb-5">
+					(Agreement: 1 Strongly Disagree 2 Disagree 3 Undecided 4 Agree 5 Strongly Agree)
+				</p>
+				<div class="flex flex-row">
+					<input type="radio" id="q31" name="q3" value="1" bind:group={q3} />
+					<label for="q31">1</label>
+					<input type="radio" id="q32" name="q3" value="2" bind:group={q3} />
+					<label for="q32">2</label>
+					<input type="radio" id="q33" name="q3" value="3" bind:group={q3} />
+					<label for="q33">3</label>
+					<input type="radio" id="q34" name="q3" value="4" bind:group={q3} />
+					<label for="q34">4</label>
+					<input type="radio" id="q35" name="q3" value="5" bind:group={q3} />
+					<label for="q35">5</label>
+				</div>
+			</div>
+
+			<a href="#comps">
+				<button
+					class="border border-white my-20 font-bold mx-5 md:ml-10 w-full h-12 text-white"
+					type="submit">Show me how I compare to others</button
+				>
+			</a>
+		</form>
+	</div>
+</div>
+
+<style>
+	input[type='radio'] {
+		display: none;
+	}
+	input[type='radio'] + label {
+		border: 2px solid white;
+
+		cursor: pointer;
+		display: block;
+		height: 62px;
+		min-width: 80px;
+		text-align: center;
+		line-height: 62px;
+		margin-right: 0.5em;
+		border-radius: 5px;
+	}
+	input[type='radio']:checked + label {
+		border: 2px solid white;
+		background-color: white;
+		color: black;
+	}
+	input[type='number'] {
+		width: 90px;
+		border: 0px solid black;
+		border-bottom: solid black;
+		text-align: center;
+	}
+	input[type='email'] {
+		width: 100%;
+		border: 0px solid black;
+		border-bottom: solid black;
+		text-align: center;
+	}
+</style>
