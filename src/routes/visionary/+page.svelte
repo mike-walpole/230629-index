f06b1cad57927b91e365f59585776133cca295c2
mike-walpole <walpole.mike@gmail.com>
2023-08-22 11:19:29 +0200

    descriptions

---
+++ src/routes/visionary/+page.svelte
@@ -0,0 +1,105 @@
+<section class="bg-[#1D1C1C] pt-24">
+	<div class="w-11/12 md:w-3/4 mx-auto">
+		<img class="w-32 mt-5" src="/digitalindexbluepineapple.webp" alt="logo" />
+	</div>
+	<div class="w-11/12 md:w-3/4 mx-auto border-t-[1px] border-white mt-32">
+		<div class="grid grid-cols-1 md:grid-cols-2 gap-x-24 mt-5">
+			<div class="col-span-1 md:w-2/3 text-white font-heldane text-5xl pl-2 mb-5 md:mb-0">
+				How do I compare
+			</div>
+			<div class="text-white font-soehne col-span-1">
+				<img class="w-full my-5 hover:grayscale-0" src="/Visionary.png" loading="lazy" alt="one" />
+				<p class=" text-white font-soehne">
+					'Visionaries' are the dreamers of the industry. These organizations are characterized by
+					their ambitious, forward-thinking strategies and plans. They possess a unique ability to
+					peer into the horizon and identify opportunities or threats long before they become
+					apparent to others. Their brainstorming sessions might be filled with blue-sky thinking,
+					innovative solutions, and disruptive ideas. However, the challenge lies in turning these
+					grand ideas into tangible results. Whether it's due to resource constraints, resistance to
+					change internally, or simply a lack of operational robustness, these organizations often
+					grapple with translating their visions into reality. Their journey is marked by peaks of
+					innovative breakthroughs and valleys of operational setbacks.
+				</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="w-11/12 md:w-3/4 mx-auto border-t-[1px] border-white mt-32">
+		<div class="grid grid-cols-1 md:grid-cols-2 gap-x-24 mt-5">
+			<div class="col-span-1 md:w-2/3 text-white font-heldane text-5xl pl-2 mb-5 md:mb-0">
+				What should I do next
+			</div>
+			<div class="text-white font-soehne col-span-1">
+				<ul class="mb-3">
+					<span class="underline">Operational Streamlining: </span> Invest in tools, processes, and training
+					to transform visionary ideas into actionable projects.
+				</ul>
+				<ul class="mb-3">
+					<span class="underline">Partnerships:</span> Consider partnerships or collaborations to bolster
+					operational capabilities.
+				</ul>
+				<ul class="mb-3">
+					<span class="underline">Feedback Loops:</span> Implement regular feedback mechanisms to understand
+					operational bottlenecks.
+				</ul>
+				<ul class="mb-3">
+					<span class="underline">Resource Allocation:</span> Ensure adequate resources (time, money,
+					personnel) are dedicated to key projects to bring visions to life.
+				</ul>
+				<ul class="mb-3">
+					<span class="underline">Change Management: </span> Cultivate an internal culture that embraces
+					change and is adaptable to shifting plans.
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="w-11/12 md:w-3/4 mx-auto border-t-[1px] border-white mt-32">
+		<div class="grid grid-cols-1 md:grid-cols-2 gap-x-24 mt-5">
+			<div class="col-span-1 md:w-2/3 text-white font-heldane text-5xl pl-2 mb-5 md:mb-0">
+				Book a complimentary insights call
+			</div>
+			<div class="text-white font-soehne col-span-1">
+				<!-- Calendly inline widget begin -->
+				<div
+					class="calendly-inline-widget"
+					data-url="https://calendly.com/bluepineapple-mike/30min?hide_event_type_details=1&hide_gdpr_banner=1&background_color=1d1c1c&text_color=ffffff&primary_color=ffffff"
+					style="min-width:320px;height:700px;"
+				/>
+				<script
+					type="text/javascript"
+					src="https://assets.calendly.com/assets/external/widget.js"
+					async
+				></script>
+				<!-- Calendly inline widget end -->
+			</div>
+		</div>
+	</div>
+</section>
+
+<div>
+	<div class="h-[23px] mt-[1px] bg-[#1D1C1C]" />
+	<div class="h-[22px] mt-[2px] bg-[#1D1C1C]" />
+	<div class="h-[21px] mt-[3px] bg-[#1D1C1C]" />
+	<div class="h-[20px] mt-[4px] bg-[#1D1C1C]" />
+	<div class="h-[19px] mt-[5px] bg-[#1D1C1C]" />
+	<div class="h-[18px] mt-[6px] bg-[#1D1C1C]" />
+	<div class="h-[17px] mt-[7px] bg-[#1D1C1C]" />
+	<div class="h-[16px] mt-[8px] bg-[#1D1C1C]" />
+	<div class="h-[15px] mt-[9px] bg-[#1D1C1C]" />
+	<div class="h-[14px] mt-[10px] bg-[#1D1C1C]" />
+	<div class="h-[13px] mt-[11px] bg-[#1D1C1C]" />
+	<div class="h-[12px] mt-[12px] bg-[#1D1C1C]" />
+	<div class="h-[11px] mt-[13px] bg-[#1D1C1C]" />
+	<div class="h-[10px] mt-[14px] bg-[#1D1C1C]" />
+	<div class="h-[9px] mt-[15px] bg-[#1D1C1C]" />
+	<div class="h-[8px] mt-[16px] bg-[#1D1C1C]" />
+	<div class="h-[7px] mt-[17px] bg-[#1D1C1C]" />
+	<div class="h-[6px] mt-[18px] bg-[#1D1C1C]" />
+	<div class="h-[5px] mt-[19px] bg-[#1D1C1C]" />
+	<div class="h-[4px] mt-[20px] bg-[#1D1C1C]" />
+	<div class="h-[3px] mt-[21px] bg-[#1D1C1C]" />
+	<div class="h-[2px] mt-[22px] bg-[#1D1C1C]" />
+	<div class="h-[1px] mt-[23px] bg-[#1D1C1C]" />
+	<div class="h-[0px] mt-[24px] bg-[#1D1C1C]" />
+</div>
